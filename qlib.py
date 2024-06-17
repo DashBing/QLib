9b29afff2c3f4c77e6560f900e22e067ed920b38
DashBing <mcbbkf@outlook.com>
2024-06-17 22:03:14 +0800

    add new gates

--- qlib.py
+++ qlib.py
@@ -17,6 +17,10 @@ def matrix_mul_vector(matrix:sympy.Matrix, vector:sympy.Matrix):
     k = sympy.symbols("k")
     return(sympy.Matrix([sympy.summation(matrix[i,k]*vector[k,0], (k, 0, vector.shape[0]-1)) for i in range(matrix.shape[0])]))
 
+def H_gate(input:QBit) -> QBit:
+    tmp = sympy.sqrt(2)**(-1) * matrix_mul_vector(sympy.Matrix([[1, 1], [1, -1]]), input.matrix)
+    return(QBit(tmp[0, 0], tmp[1, 0]))
+
 def X_gate(input:QBit) -> QBit:
     tmp = matrix_mul_vector(sympy.Matrix([[0, 1], [1, 0]]), input.matrix)
     return(QBit(tmp[0, 0], tmp[1, 0]))
@@ -29,11 +33,20 @@ def Z_gate(input:QBit) -> QBit:
     tmp = matrix_mul_vector(sympy.Matrix([[1, 0], [0, -1]]), input.matrix)
     return(QBit(tmp[0, 0], tmp[1, 0]))
 
-def H_gate(input:QBit) -> QBit:
-    tmp = sympy.sqrt(2)**(-1) * matrix_mul_vector(sympy.Matrix([[1, 1], [1, -1]]), input.matrix)
+def RX_gate(input:QBit, theta:float|sympy.Rational) -> QBit:
+    tmp = matrix_mul_vector(sympy.Matrix([[sympy.cos(theta/2), -sympy.I*sympy.sin(theta/2)], [-sympy.I*sympy.sin(theta/2), sympy.cos(theta/2)]]), input.matrix)
+    return(QBit(tmp[0, 0], tmp[1, 0]))
+
+def RY_gate(input:QBit, theta:float|sympy.Rational) -> QBit:
+    tmp = matrix_mul_vector(sympy.Matrix([[sympy.cos(theta/2), -sympy.sin(theta/2)], [sympy.sin(theta/2), sympy.cos(theta/2)]]), input.matrix)
+    return(QBit(tmp[0, 0], tmp[1, 0]))
+
+def RZ_gate(input:QBit, theta:float|sympy.Rational) -> QBit:
+    tmp = matrix_mul_vector(sympy.Matrix([[sympy.exp(-sympy.I*theta/2), 0], [0, sympy.exp(-sympy.I*theta/2)]]), input.matrix)
     return(QBit(tmp[0, 0], tmp[1, 0]))
 
 if __name__ == "__main__":
     print(sympy.expand_func(matrix_mul_vector(sympy.Matrix([[2,3],[1,-5]]), sympy.Matrix([[4],[1]]))))
     print(X_gate(QBit(1,0)))
     print(H_gate(QBit(1,0)))
+    print(RZ_gate(QBit(1,0),sympy.pi/4))
